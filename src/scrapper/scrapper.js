ddcb412a86bdfaacbaa1df4a5618f0a06a68d474
Abdul-fatai <staticdev20046@gmail.com>
2020-09-26 22:03:41 +0100

    API ready

--- src/scrapper/scrapper.js
+++ src/scrapper/scrapper.js
@@ -1,6 +1,51 @@
 const request = require('request');
 const cheerio = require('cheerio');
 
+// Free Tutorials US
+const freetutorialsus = async (title) => {
+  console.log(title);
+  return new Promise((resolve, reject) => {
+    const newTitle = title.replace(/\s+/g, '+');
+    const url = `https://www.freetutorialsus.com/?s=${newTitle}`;
+    let options = {
+      url: url,
+      headers: {
+        'User-Agent':
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
+      },
+    };
+
+    request(options, (error, response, html) => {
+      if (!error && response.statusCode === 200) {
+        const $ = cheerio.load(html);
+        let json = {
+          results: [],
+          version: require('../../package.json').version,
+        };
+
+        $('.post-box').each((i, el) => {
+          const id = i;
+          const title = $(el).find('.post-title').text();
+          const link = $(el).find('.readmore a').attr('href');
+          const image = $(el).find('.wp-post-image').attr('src');
+          const date = $(el).find('.entry-date').text();
+          json.results.push({ id, title, image, link, date });
+        });
+
+        // Check is Result is []
+        if (json.results.length === 0) {
+          freecoursesite(title);
+        }
+
+        return resolve(json);
+      }
+
+      resolve({ error: error });
+    });
+  });
+};
+
+// Free Course Site
 const freecoursesite = async (title) => {
   return new Promise((resolve, reject) => {
     const newTitle = title.replace(/\s+/g, '+');
@@ -43,41 +88,4 @@ const freecoursesite = async (title) => {
   });
 };
 
-const freetutorialsus = async (title) => {
-  return new Promise((resolve, reject) => {
-    const newTitle = title.replace(/\s+/g, '+').toLowerCase();
-    const url = `https://www.freetutorialsus.com/?s=${newTitle}`;
-    let options = {
-      url: url,
-      headers: {
-        'User-Agent':
-          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
-      },
-    };
-
-    request(options, (error, response, html) => {
-      if (!error && response.statusCode === 200) {
-        const $ = cheerio.load(html);
-        let json = {
-          results: [],
-          version: require('../../package.json').version,
-        };
-
-        $('.post-box').each((i, el) => {
-          const id = i;
-          const title = $(el).find('.post-title').text();
-          const link = $(el).find('.readmore a').attr('href');
-          const image = $(el).find('.wp-post-image').attr('src');
-          const date = $(el).find('.entry-date').text();
-          json.results.push({ id, title, image, link, date });
-        });
-
-        return resolve(json);
-      }
-
-      resolve({ error: error });
-    });
-  });
-};
-
-module.exports.tutorial = freecoursesite;
+module.exports.tutorial = freetutorialsus;
